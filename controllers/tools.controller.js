cb9188d3eab63f81ffb2fa7f83d114825e7fa0a4
mohiuddin2721 <mohiuddinpatwary27@gmail.com>
2023-01-21 16:20:41 +0600

    insert data in mongodb successfully

--- controllers/tools.controller.js
+++ controllers/tools.controller.js
@@ -1,3 +1,5 @@
+const { getDb } = require("../utils/dbConnect")
+
 module.exports.getAllTools = (req, res) => {
     // res.send('tools found')
     // const {id, params, ip, query, body, header}= req;
@@ -9,8 +11,22 @@ module.exports.getAllTools = (req, res) => {
     // req and res have many method. We must need to explore all from express documentation
 }
 
-module.exports.saveATools = (req, res) => {
-    res.send('tools post')
+module.exports.saveATools = async (req, res, next) => {
+    try {
+        const db = getDb();
+        const tool = req.body;
+
+        const result = await db.collection('tools').insertOne(tool);
+        // console.log(result.insertedId)
+        if (!result.insertedId) {
+            return res.status(400).send({ status: false, error: 'Something went wrong' })
+        }
+        // res.send(`Tool added with id: ${result.insertedId}`)
+        res.send({success: true, message: `Tool added with id: ${result.insertedId}`}) //better way
+    } catch (error) {
+        next(error)
+    }
+
 }
 
 module.exports.getToolId = (req, res) => {
